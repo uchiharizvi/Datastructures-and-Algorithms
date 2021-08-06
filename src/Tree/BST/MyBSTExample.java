03b938d5809f433a5b6eb5a4dfe7be96765f3ff6
Kavish Rizvi <rizvikavish205@gmail.com>
2021-08-06 11:56:32 +0530

    First Commit

---
+++ src/Tree/BST/MyBSTExample.java
@@ -0,0 +1,10 @@
+package Tree.BST;
+
+public class MyBSTExample {
+    Node root = new Node();
+    public Node search(Node root, int key){
+        if(root==null||root.ke)
+
+        return search(root.key);
+    }
+}
---
+++ src/Tree/BST/Node.java
@@ -0,0 +1,13 @@
+package Tree.BST;
+
+public class Node {
+
+    private int key;
+    private Node left;
+    private Node right;
+
+    public Node(int item) {
+        key = item;
+        left=right=null;
+    }
+}
