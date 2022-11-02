10a3c192dff07e127b3a780f79036c69d449e3c1
Kavish Rizvi <rizvikavish205@gmail.com>
2022-11-02 12:23:33 +0530

    Insertion Sort Linked List

---
+++ src/Algorithms/Searching/InsertionSortOnLinkedList/LinkedList.java
@@ -0,0 +1,45 @@
+package Algorithms.Searching.InsertionSortOnLinkedList;
+
+public class LinkedList {
+    Node head;
+    Node sorted;
+
+    void push(int data) {
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+    }
+
+    void print(Node head) {
+        while (head != null) {
+            System.out.println(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    void sort(Node head) {
+        sorted = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            sortedInsert(curr);
+            curr = next;
+        }
+        head = sorted;
+    }
+
+    void sortedInsert(Node node) {
+        if (sorted == null || sorted.data >= node.data) {
+            node.next = sorted;
+            sorted = node;
+        } else {
+            Node current = sorted;
+            /* Locate the node before the point of insertion */
+            while (current.next != null && current.next.data < node.data) {
+                current = current.next;
+            }
+            node.next = current.next;
+            current.next = node;
+        }
+    }
+}
---
+++ src/Algorithms/Searching/InsertionSortOnLinkedList/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package Algorithms.Searching.InsertionSortOnLinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.push(5);
+        list.push(20);
+        list.push(4);
+        list.push(3);
+        list.push(30);
+        System.out.println("Linked List before Sorting..");
+        list.print(list.head);
+        list.sort(list.head);
+        System.out.println("\nLinkedList After sorting");
+        list.print(list.head);
+
+    }
+}
